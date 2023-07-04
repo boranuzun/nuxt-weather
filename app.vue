0c1cabfc52190e86c39dabe886567af6391bdb92
Boran <boran.u3@gmail.com>
2023-07-04 17:39:38 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <Html lang="en" dir="ltr">
+    <Head>
+      <Title>Weather app</Title>
+      <Link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Link>
+    </Head>
+
+    <div class="flex flex-col min-h-screen">
+      <SiteHeader />
+      <main
+        class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full"
+      >
+        <NuxtPage />
+      </main>
+      <SiteFooter />
+    </div>
+  </Html>
+</template>
